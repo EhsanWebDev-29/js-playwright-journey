5a1bfe98161ac4571550c07fd5db7a841065a00b
EhsanWebDev-29 <ehsan.webdev29@gmail.com>
2026-09-05 22:12:09 +0530

    Day 6: primitive vs reference, typeof, coercion, equality

---
+++ day6-Data-types/datatypes.js
@@ -0,0 +1,14 @@
+
+// Primitive
+
+let number = 5;
+let num2 = number;
+num2=8;
+console.log(number);
+
+// non-primitive
+
+let obj1 = {age:25};
+let obj2 = obj1;
+obj2.age = 30;
+console.log(obj1);
